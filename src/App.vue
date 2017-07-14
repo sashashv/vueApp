3d4b24b15ed897d584c4bb2a2c5b1ee48ecbc241
Galyna Popova <galka_po@mail.ru>
2017-07-14 21:16:55 +0300

    test

--- src/App.vue
+++ src/App.vue
@@ -1,11 +1,21 @@
 <template>
   <div id="app">
-  <nav>
-    <router-link to="/"><img src="./assets/logo.png"></router-link>
-    <router-link to="/admin">Admin</router-link>
-    <router-link to="/login">Login</router-link>
-    <router-view></router-view>
-  </nav>
+    <nav>
+      <ul class="nav-wrapper">
+        <li>
+          <router-link to="/"><img src="./assets/logo.png"></router-link>
+        </li>
+        <li>
+          <router-link to="/admin">Admin</router-link>
+        </li>
+        <li>
+          <router-link to="/login">Login</router-link>
+        </li>
+      </ul>
+    </nav>
+    <div class="main">
+      <router-view></router-view>
+    </div>
   </div>
 </template>
 
@@ -15,16 +25,41 @@ export default {
 }
 </script>
 
-<style lang="sass">
-#app
-  font-family: 'Avenir', Helvetica, Arial, sans-serif
-  -webkit-font-smoothing: antialiased
-  -moz-osx-font-smoothing: grayscale
-  text-align: center
-  color: #2c3e50
-  margin-top: 60px
-  img
-    width: 50px
-    height: 50px
+<style lang="scss" scoped>
+#app {
+  font-family: 'Avenir', Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+
+  img{
+    width: 30px;
+    height: 30px;
+  }
+  ul {
+    background-color: #ccc;
+    background: rgba(240, 231, 253, 0.5);
+    display: flex;
+    justify-content: space-around;
+    width: 100vw;
+    border: 0px solid black;
+    margin: 0;
+    padding: 0;
+    height: 50px;
+
+    li {
+      display: flex;
+      list-style: none;
+      justify-content: center;
+      justify-items: center;
+      height: 50px;
+      flex-direction: column;
+      a {
+        display: block;
+      }
+    }
+  }
+}
 
 </style>
